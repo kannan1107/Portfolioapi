378fa09cb95516c63ad83d7826678cccc80c491b
kannan1107 <kannan11071985@gmail.com>
2026-08-23 14:06:29 +0530

    update

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./src/Config/db.js";
 import { errorHandler } from "./src/middleware/errorHandler.js";
 import QueryRouter from "./src/Router/QueryRouter.js";
 
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://kannanmportfolio.netlify.app",
-    process.env.FRONTEND_URL,
-  ],
-  methods: ["GET", "POST", "OPTIONS"],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "https://portfolio-kuq5.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 
 app.use("/api", QueryRouter);
@@ -26,5 +24,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  connectDB();
 });
